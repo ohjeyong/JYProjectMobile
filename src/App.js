a1d878e0903eb76c30711ec4f313c67741b29497
JeyongOh <jeyong.oh@oshare.kr>
2017-06-08 18:03:22 +0900

    make app.js to sync android and ios

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/**
+ * Created by oh on 2017. 6. 8..
+ */
+import React from 'react';
+import { Text } from 'react-native';
+
+const App = () => (
+    <Text>Hello World!</Text>
+);
+
+export default App;
